44bbf0d4a706ef604a92d3d54ac1e01c30515dae
rifqi2242 <rifqimuzakki45@gmail.com>
2023-12-16 12:35:38 +0700

    add dosen api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mahasiswaController = require('./controllers/mahasiswacontroller');
-
+const dosenController = require('./controllers/dosenController'); 
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use('/mahasiswa', mahasiswaController);
+app.use('/dosen', dosenController);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
